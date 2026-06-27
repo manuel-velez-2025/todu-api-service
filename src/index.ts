a7d6e7c12c9c7115521291ac26306b827eed4803
Ari1229-jj <243698@ids.upchiapas.edu.mx>
2026-06-27 13:53:53 -0600

    feat(api): implementar endpoint post /api/tareas y persistencia en postgres

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+import 'dotenv/config';
 import express from 'express';
 import rutas from './infrastructure/http/routes';
 
 const app = express();
 app.use(express.json());
+
 app.use('/api', rutas);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Todú API corriendo en http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log(' Todú API corriendo en http://localhost:3000');
 });
